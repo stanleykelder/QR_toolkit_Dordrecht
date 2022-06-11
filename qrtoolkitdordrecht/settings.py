eb411a007785cd2fd3414f25384aa8da7cfaaecc
stanleykelder <stanleykelder@gmail.com>
2022-06-11 18:26:43 +0200

    static folder location?

--- qrtoolkitdordrecht/settings.py
+++ qrtoolkitdordrecht/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 
 # Static files when used in production 
 # https://devcenter.heroku.com/articles/django-assets
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'qrtoolkit_core/static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
