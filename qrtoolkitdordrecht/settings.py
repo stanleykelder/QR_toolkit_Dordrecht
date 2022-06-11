35d48d67dc9efde8184454f91fc628a12d73e147
stanleykelder <stanleykelder@gmail.com>
2022-06-11 18:34:31 +0200

    static

--- qrtoolkitdordrecht/settings.py
+++ qrtoolkitdordrecht/settings.py
@@ -137,12 +137,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'qrtoolkit_core/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'qrtoorlkit_core/static'),
+    os.path.join(BASE_DIR, 'qrtoorlkit_core'),
 )
 
 # # Activate Django-Heroku.
